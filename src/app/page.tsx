f4e06d86bc2a389a016da4415a73bd076330eb92
smitgol <60008621+smitgol@users.noreply.github.com>
2023-11-07 19:25:16 +0530

    removed handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import Image from 'next/image'
-import {handler} from "../../functions/enqueueTest"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
